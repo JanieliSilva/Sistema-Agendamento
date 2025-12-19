721ec9e66412760f31fa2bc5ecf7e3212962a116
Janieli Tainar <tainasilver824@gmail.com>
2025-12-19 10:41:41 -0300

    "fix: resolve imports dos repositorios"

---
+++ src/main/java/com/salao/agendamento/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.salao.agendamento.repository;
+
+import com.salao.agendamento.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
